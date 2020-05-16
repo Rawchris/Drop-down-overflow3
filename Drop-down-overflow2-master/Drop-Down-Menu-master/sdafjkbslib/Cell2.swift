e11d9d1bc9674f44532b519c281f36408885c906
Rawchris <50545792+Rawchris@users.noreply.github.com>
2020-05-16 10:41:24 +0200

    Add files via upload

---
+++ Drop-down-overflow2-master/Drop-Down-Menu-master/sdafjkbslib/Cell2.swift
@@ -0,0 +1,24 @@
+//
+//  Cell2.swift
+//  sdafjkbslib
+//
+//  Created by Christoffer Mouritsen on 16/05/2020.
+//  Copyright © 2020 Archetapp. All rights reserved.
+//
+
+import UIKit
+
+class Cell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Drop-down-overflow2-master/Drop-Down-Menu-master/sdafjkbslib/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  sdafjkbslib
+//
+//  Created by Davidson Family on 11/1/17.
+//  Copyright © 2017 Archetapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Configure the button
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return CGFloat(200)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+            cell.selectionStyle = UITableViewCell.SelectionStyle.none
+            cell.setTableViewCell()
+            
+            cell.clipsToBounds=false
+            cell.contentView.clipsToBounds=false
+            cell.layer.zPosition = 3
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2") as! Cell2
+            return cell
+        }
+    }
+    
+}
+
